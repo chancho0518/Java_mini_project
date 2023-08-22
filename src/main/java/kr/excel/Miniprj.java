3b7bc937b7c619e875a6e2df567359e1d293b1f9
이충열 <chancho0518@gmail.com>
2023-08-22 18:58:52 +0900

    셀 타입에 대한 내용 추가

--- src/main/java/kr/excel/Miniprj.java
+++ src/main/java/kr/excel/Miniprj.java
@@ -5,6 +5,8 @@ import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.*;
+import java.util.Date;
 
 public class Miniprj {
     public static void main(String[] args) {
@@ -15,12 +17,52 @@ public class Miniprj {
 
             for(Row row : sheet) {
                 for(Cell cell: row) {
-                    System.out.print(cell.toString() + "\t" );
+                    switch (cell.getCellType()) {
+                        case NUMERIC:
+                            if(DateUtil.isCellDateFormatted(cell)) {
+                                Date dateValue = cell.getDateCellValue();
+                                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                String fomattedDate = dateFormat.format(dateValue);
+                                System.out.print(fomattedDate + "\t");
+                            } else {
+                                double numbericValue = cell.getNumericCellValue();
+                                if(numbericValue == Math.floor(numbericValue)) {
+                                    int intValue = (int) numbericValue;
+                                    System.out.print(intValue + "\t");
+                                } else {
+                                    System.out.print(numbericValue + "\t");
+                                }
+                            }
+                            break;
+
+                        case STRING:
+                            String stringValue = cell.getStringCellValue();
+                            System.out.print(stringValue + "\t");
+                            break;
+
+                        case BOOLEAN:
+                            Boolean booleanValue = cell.getBooleanCellValue();
+                            System.out.print(booleanValue + "\t");
+                            break;
+
+                        case FORMULA:
+                            String formula = cell.getCellFormula();
+                            System.out.print(formula + "\t");
+                            break;
+
+                        case BLANK:
+                            System.out.print("\t");
+                            break;
+
+                        default:
+                            System.out.print("\t");
+                            break;
+                    }
                 }
                 System.out.println();
             }
             file.close();
-            System.out.println("Excel Data 읽어오기 성공!!");
+            System.out.println("\n" + "Excel Data 읽어오기 성공!!");
 
         } catch (IOException e) {
             e.printStackTrace();
